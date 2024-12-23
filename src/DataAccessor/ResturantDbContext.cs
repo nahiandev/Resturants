ddecce56d55fc9aa20d7e3f3a8665f75eaaaaa50
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-23 23:36:55 +0600

    Add DbContext, entities, and initial migration
    Updated `ResturantDbContext` to include a constructor with `DbContextOptions` and added `DbSet` properties for `Resturant` and `Dish`. Overrode `OnModelCreating` to configure relationships, including the owned `Address` entity and one-to-many relationship between `Resturant` and `Dish`.
    
    Added `ResturantId` property to `Dish` class to establish a foreign key relationship.
    
    Created migration `20241223171452_0xEVQZ7S` to define schema changes, creating `Resturants` and `Dishes` tables with appropriate columns and constraints.
    
    Generated `20241223171452_0xEVQZ7S.Designer.cs` to snapshot the model's state at migration time.
    
    Updated `ResturantDbContextModelSnapshot.cs` to reflect current model state.

--- src/DataAccessor/ResturantDbContext.cs
+++ src/DataAccessor/ResturantDbContext.cs
@@ -1,8 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Models.Domains;
 
 namespace Restaurants.DataAccessor
 {
     public class ResturantDbContext : DbContext
     {
+        public ResturantDbContext(DbContextOptions<ResturantDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Resturant> Resturants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+
+        //    optionsBuilder.UseSqlServer("Server=localhost;Database=Resturants;User Id=sa;Password=Password123;");
+        //}
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+
+            model.Entity<Resturant>().OwnsOne(r => r.Address);
+
+            model.Entity<Resturant>().HasMany(r => r.Dishes)
+                .WithOne().HasForeignKey(d => d.ResturantId);
+        }
     }
 }
