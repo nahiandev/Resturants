b3e7ffb71ffdd5a3acb83271c4ef29590b2141f8
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-23 22:49:53 +0600

    Dish

--- src/Models/Domains/Dish.cs
+++ src/Models/Domains/Dish.cs
@@ -2,5 +2,9 @@
 {
     public class Dish
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
