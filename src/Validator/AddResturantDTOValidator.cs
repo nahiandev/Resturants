e5fbf1dbcb5b52aff239eb23cb71d958d41b8aaa
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-24 23:44:58 +0600

    Add FluentValidation and update EF Core packages
    Updated Restaurants.csproj to include FluentValidation.AspNetCore 11.3.0 for validation in ASP.NET Core. Upgraded Microsoft.EntityFrameworkCore and Microsoft.EntityFrameworkCore.Design to 9.0.0. Added AddResturantDTOValidator to enforce validation rules on AddResturantDTO.

---
+++ src/Validator/AddResturantDTOValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Restaurants.Models.DTOs;
+
+namespace Restaurants.Validator
+{
+    public class AddResturantDTOValidator : AbstractValidator<AddResturantDTO>
+    {
+        public AddResturantDTOValidator()
+        {
+            RuleFor(r => r.Name).NotEmpty().MinimumLength(3).
+                MaximumLength(50).WithMessage("Name must be between 3 and 50 characters.");
+        }
+    }
+}
