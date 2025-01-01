c5bf90c37f66649d5d6074eb2ad48f9fff3f44b1
Ibne Nahian <evilprince2009@yahoo.com>
2025-01-01 18:07:09 +0600

    Add DbSet<Dish> and configure entity relationships
    Updated using directives in ResturantDbContext.cs to include Restaurants.Domain.Entities and Restaurants.Domains.Models. Added internal DbSet<Dish> property. Implemented OnModelCreating method to configure entity relationships: Resturant owns one Address and has a one-to-many relationship with Dish, with Dish having a foreign key RestaurantId.

--- src/DataAccessor/ResturantDbContext.cs
+++ src/DataAccessor/ResturantDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
 using Restaurants.Domains.Models;
 
 namespace Restaurants.DataAccessor
@@ -10,12 +11,26 @@ namespace Restaurants.DataAccessor
         }
 
         public DbSet<Resturant> Resturants { get; init; }
+        internal DbSet<Dish> Dishes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
 
             optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Resturant>()
+                .OwnsOne(r => r.Address);
+
+            modelBuilder.Entity<Resturant>()
+                .HasMany(r => r.Dishes)
+                .WithOne()
+                .HasForeignKey(d => d.RestaurantId);
         }
     }
 }
